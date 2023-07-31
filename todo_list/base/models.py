93ed90d66a25406ef8df2690619aefe278805fed
Gavriil1 G <airbnbguide@gmail.com>
2023-07-31 16:29:04 +0000

    created first table made migration and completed migration

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -9,4 +9,10 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=T)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
