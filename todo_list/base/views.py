6bf36e5a20a86e64d4267fd6d9a53dc74ee37a10
Gavriil1 G <airbnbguide@gmail.com>
2023-08-02 17:12:07 +0000

    rename task_detail to task. Compleated class detail view. Linked this view to task_list.html. Verified that everything works well

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -8,8 +8,11 @@ class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
+
 class TaskDetail(DetailView):
     model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
 
 
 
