e88576c9c49f2c399a1d7f34cdcda92ba6a5b44d
Gavriil1 G <airbnbguide@gmail.com>
2023-08-13 10:36:35 +0000

    created elephanturl link added it to settings. run the migration

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,12 +77,17 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse('postgres://ubclqibd:CixAseEG0IhtJKZlKxW5y745YI3YDQfn@trumpet.db.elephantsql.com/ubclqibd')
 }
+
 
 
 # Password validation
