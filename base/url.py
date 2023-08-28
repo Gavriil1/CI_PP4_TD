a5b004cf4682a334b4c5894ad3af836b5e815b32
Gavriil1 G <airbnbguide@gmail.com>
2023-08-28 15:49:17 +0000

    added a script which gives an option to a customer to add a task, when task list is empty. Created 4 htmls to create view only for dayly weekly montlhy yearly view

--- base/url.py
+++ base/url.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
-    path('yo/', TaskList.as_view(), name='tasks'),
+    path('yo/', TaskList.as_view(), name='tasks1'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
-    path('', testing, name='testing'),  
+    path('', testing, name='tasks'),  
 ]
 
