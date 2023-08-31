0a221d4745f1253a75f82272b59652be4040ea8c
Gavriil1 G <airbnbguide@gmail.com>
2023-08-31 15:41:38 +0000

    started adding additional field s to admin views

--- base/admin.py
+++ base/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Task
 # Register your models here.
 
-admin.site.register(Task)
+# admin.site.register(Task)
+@admin.register(Task)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'importance']
