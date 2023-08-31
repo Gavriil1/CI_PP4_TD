10beebaa642cb00875ca02f951e6b75653b53061
Gavriil1 G <airbnbguide@gmail.com>
2023-08-31 15:48:03 +0000

    added new view to admin

--- base/admin.py
+++ base/admin.py
@@ -5,5 +5,21 @@ from .models import Task
 # admin.site.register(Task)
 @admin.register(Task)
 
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'importance']
+class AdminTaskView(admin.ModelAdmin):
+    list_display = ['title', 'description', 'importance', 'complete']
+    list_filter = ['importance', 'frequency','user']
+    search_fields = ['description']
+
+
+
+
+
+
+#   user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+#     title = models.CharField(max_length=200)
+#     description = models.TextField(null=True, blank=True)
+#     complete = models.BooleanField(default=False)
+#     created = models.DateTimeField(auto_now_add=True)
+#     frequency = models.CharField(max_length=10, choices=FREQUENCY_CHOICES, default='Daily')
+#     importance = models.CharField(max_length=10, choices=Important_Choice, default='Urgent')
+#     completed = models.CharField(max_length=10, choices=COMPLETED_CHOICE, default='InComplete')
