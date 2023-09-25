08a9ac0fa71fbbd82f5f89ff827fa00a2e60ac33
Gavriil1 G <airbnbguide@gmail.com>
2023-09-25 05:11:28 +0000

    added three tests for homepage, manua, login views.

--- base/test/test_urls.py
+++ base/test/test_urls.py
@@ -1,7 +1,26 @@
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from base.views import CustomLoginView, RegisterPage, TaskDetail, TaskCreate, TaskUpdate, DeleteView, TaskList, homepage, manual
 
 
 class TestUrls(SimpleTestCase):
 
-    def test_list_url_is_resolved(self):
-        assert 1 == 2
+    def test_list_url_is_resolved_homepage(self):
+        url = reverse('tasks')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, homepage)
+
+    def test_list_url_is_resolved_manual(self):
+        url = reverse('manual')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, manual)
+
+    def test_list_url_is_resolved_login(self):
+        url = reverse('login')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, CustomLoginView)
+
+
+    
+
+
