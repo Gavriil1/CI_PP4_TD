48c0ebe133b73a9625d4dde0b7d727d56e8ad97a
Gavriil1 G <airbnbguide@gmail.com>
2023-10-01 23:50:22 +0000

    added 404 page

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = "SECRET_KEY"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -148,9 +148,13 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+
+# STATIC_ROOT = BASE_DIR / 'base/static'
+
+
+# STATIC_ROOT = BASE_DIR / '/workspace/CI_PP4_TD/base/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
